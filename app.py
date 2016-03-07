3e2ff98b4c5531ed83fe8571522f3c709b786b3a
root <shan.tulshi@gmail.com>
2016-03-06 22:37:29 -0500

    added new endpoint which takes an argument, "email"

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
 	return 'Hello, world!'
 
+@app.route('/new')
+def newEmail():
+	if 'email' in request.args:
+		return 'endpoint working???'
+	else: 	return 'Invalid request! Argument "email" missing!'
+
 if __name__ == '__main__':
+	app.debug=True
 	app.run('0.0.0.0')
 
